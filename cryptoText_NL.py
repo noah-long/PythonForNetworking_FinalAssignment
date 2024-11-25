9db780335359b869b1ec81cb7eb1b036f295ce17
noah-long <noah.long@mohawkcollege.ca>
2024-11-25 14:55:47 -0500

    Created cryptoText_NL.py
    Created file, import modules

---
+++ cryptoText_NL.py
@@ -0,0 +1,8 @@
+# Text encryption/decryption - Final Assignment
+# Create a command line interface where you can supply a message and a key value to encrypt the message using the Python cryptography module. 
+# The message will be written to a file of the user's choice. 
+# The script must be able to read text from a specified file and be able to decrypt a message using a supplied key value.
+#
+import os
+import cryptography 
+import argparse
